96b75cee5d29a839fe0cb53bdfd7a0e90cb0fa0b
broda1802 <bebe1802@o2.pl>
2021-11-17 15:28:36 +0100

    main_page

--- the_good_hands/admin.py
+++ the_good_hands/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from the_good_hands.models import Category, Institution, Donation
 
-admin.register(Category)
-admin.register(Institution)
-admin.register(Donation)
+admin.site.register(Category)
+admin.site.register(Institution)
+admin.site.register(Donation)
--- turn_over_to_the_good_hands/urls.py
+++ turn_over_to_the_good_hands/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 from the_good_hands.views import LandingPage, AddDonation
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', LandingPage.as_view(), name='landing_page'),
     path('add_donation', AddDonation.as_view(), name='add_donation'),
 ]
